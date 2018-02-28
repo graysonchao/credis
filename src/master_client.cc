ae147291b9b6aaa275bbee92c313e08d4e46b3e0
Grayson Chao <grayson.chao@gmail.com>
2018-02-27 21:05:44 -0800

    Member: Watch own config and process updates
    Updates the member module so that it implements the following:
    
    1. Write own connection info and current config in etcd.
        When connected to a cluster, the member now reports Redis
    connection info and its current prev/next/role. When these configuration
    values change, they are updated in etcd.
    
    2. Watch configs pushed by the master and update accordingly.
        The member listens to a stream of changes to its own intended
    configuration, which the master writes to etcd. When a change happens,
    it sends the appropriate invocation of MEMBER.SET_ROLE to _itself_ in
    order to make the change happen.
    
    3. New parent blocks until its new child has reported updated config
        As discussed in the CR paper, the parent must begin to forward
    writes _after_ the child begins to forward acks. To ensure this, a new
    parent waits until it sees itself reported as its child's parent. See
    design doc and CR page 5 under "Failure of other servers." As the paper
    calls it, this is maintaining the "Update Propagation Invariant."

--- src/master_client.cc
+++ src/master_client.cc
@@ -2,10 +2,14 @@
 #include <string>
 #include <thread>
 
-#include "master_client.h"
+#include "nlohmann/json.hpp"
 #include "glog/logging.h"
+#include "hiredis/hiredis.h"
+
+#include "master_client.h"
 
 using namespace etcd;
+using json = nlohmann::json;
 
 /**
  * Connect to the master.
@@ -18,17 +22,23 @@ using namespace etcd;
 Status MasterClient::Connect(
     const std::string& address,
     const int port,
-    const std::string& chain_id
+    const std::string& chain_id,
+    const std::string& redis_addr,
+    const int redis_port
 ) {
-  auto channel = grpc::CreateChannel(address + ":" + std::to_string(port), grpc::InsecureChannelCredentials());
-  etcd_client_ = std::unique_ptr<EtcdClient>(new EtcdClient(channel));
+  channel_ = grpc::CreateChannel(address + ":" + std::to_string(port), grpc::InsecureChannelCredentials());
+  etcd_client_ = std::unique_ptr<EtcdClient>(new EtcdClient(channel_));
   chain_id_ = chain_id;
+  redis_addr_ = redis_addr;
+  redis_port_ = redis_port;
   // Start a lease with etcd. Sending a KeepAlive for this lease implements a heartbeat.
-  StartHeartbeat(channel);
+  StartHeartbeat();
   // While holding a chain-wide lock, acquire the next available chain member ID and claim it for myself.
-  AcquireID(chain_id_);
+  AcquireID();
+  // Monitor changes to my intended state.
+  StartWatchingConfig();
   // Register myself as a member of the chain by putting my member ID as a key. The key disappears if heartbeat expires.
-  RegisterAsMember(chain_id_);
+  RegisterMemberInfo();
   return Status::OK();
 }
 
@@ -43,7 +53,7 @@ const std::string MasterClient::WatermarkKey(std::string chain_id, Watermark w) 
 }
 
 /**
- * Return the name of the key containing the last assigned ID for the given chain.
+ * Return the key containing the last assigned ID for the given chain.
  * @param chain_id
  * @return
  */
@@ -60,9 +70,24 @@ const std::string MasterClient::JoinLock(std::string chain_id) const {
   return kKeyPrefix + chain_id + "/_join";
 }
 
-const std::string MasterClient::ConnInfoKey(std::string chain_id) const {
-  CHECK(member_id_ != kUnsetMemberID) << "Member can't get its connection info key if it doesn't know its member ID.";
-  return kKeyPrefix + chain_id + "/" + std::to_string(member_id_);
+/**
+ * Return the heartbeat key for this member
+ * @param chain_id
+ * @return
+ */
+const std::string MasterClient::HeartbeatKey(std::string chain_id, int64_t member_id) const {
+  CHECK(member_id_ != kUnsetMemberID) << "Member can't get its heartbeat key if it doesn't know its member ID.";
+  return kKeyPrefix + chain_id + "/" + std::to_string(member_id) + "/hb";
+}
+
+/**
+ * Return the config key for this member
+ * @param chain_id
+ * @return
+ */
+const std::string MasterClient::ConfigKey(std::string chain_id, int64_t member_id) const {
+  CHECK(member_id_ != kUnsetMemberID) << "Member can't get its config key if it doesn't know its member ID.";
+  return kKeyPrefix + chain_id + "/" + std::to_string(member_id) + "/config";
 }
 
 Status MasterClient::GetWatermark(Watermark w, int64_t* val) const {
@@ -73,7 +98,6 @@ Status MasterClient::GetWatermark(Watermark w, int64_t* val) const {
     auto kvs = response->kvs();
     const KeyValue &result = kvs.Get(0);
     *val = std::stol(result.value());
-    LOG(INFO) << "(etcd) GET " << WatermarkKey(chain_id_, w) << ": " << result.value();
     return Status::OK();
   } else {
     switch (w) {
@@ -96,10 +120,6 @@ Status MasterClient::SetWatermark(Watermark w, int64_t new_val) {
       true
   );
   std::string prev_value = response->prev_kv().value();
-
-  LOG(INFO) << "(etcd) PUT " << WatermarkKey(chain_id_, w) << ": " << new_val
-            << " (was " << prev_value << ")";
-
   return Status::OK();
 }
 
@@ -109,14 +129,14 @@ Status MasterClient::SetWatermark(Watermark w, int64_t new_val) {
  * It can be used with other operations to time out if we lose contact with etcd, like with the lock in AcquireID.
  * @param channel
  */
-void MasterClient::StartHeartbeat(std::shared_ptr<grpc::Channel> channel) {
+void MasterClient::StartHeartbeat() {
   // Establish heartbeat.
   auto lease_grant = etcd_client_->LeaseGrant(kHeartbeatTimeoutSec);
   heartbeat_lease_id_ = lease_grant->id();
-  heartbeat_thread_ = std::thread(
+  heartbeat_thread_ = std::unique_ptr<std::thread>(new std::thread(
       // No need to join - this should only die if the process dies.
-      [channel](int64_t lease_id, int hb_interval) {
-          auto etcd = std::unique_ptr<EtcdClient>(new EtcdClient(channel));
+      [this](int64_t lease_id, int hb_interval) {
+          auto etcd = std::unique_ptr<EtcdClient>(new EtcdClient(channel_));
           while (true) {
             etcd->LeaseKeepAlive(lease_id);
             std::this_thread::sleep_for(std::chrono::seconds(hb_interval));
@@ -124,7 +144,7 @@ void MasterClient::StartHeartbeat(std::shared_ptr<grpc::Channel> channel) {
       },
       heartbeat_lease_id_,
       kHeartbeatIntervalSec
-  );
+  ));
   LOG(INFO) << "Started heartbeat."
             << "Interval: " << kHeartbeatIntervalSec << " seconds, "
             << "Timeout: " << kHeartbeatTimeoutSec << " seconds.";
@@ -143,32 +163,22 @@ void MasterClient::StartHeartbeat(std::shared_ptr<grpc::Channel> channel) {
  * @param lease_id
  * @return
  */
-void MasterClient::AcquireID(std::string chain_id) {
+void MasterClient::AcquireID() {
   if (member_id_ != kUnsetMemberID) {
     LOG(INFO) << "Tried to join a chain but was already in one.";
     return;
   }
-  // PLEASE ensure that any time you take a lock, you include the heartbeat lease id.
-  // If the heartbeat lease is not included, and you crash while holding the lock, THE LOCK IS PERMANENTLY LOCKED.
-  auto lock_response = etcd_client_->Lock(JoinLock(chain_id), heartbeat_lease_id_);
+  // Any time we take a shared lock, we must include the heartbeat lease id.
+  // If the heartbeat lease is not included, and we crash while holding the lock, THE LOCK IS LEAKED.
+  auto lock_response = etcd_client_->Lock(JoinLock(chain_id_), heartbeat_lease_id_);
   std::string lock_id = lock_response->key();
 
-  // RequestOp takes ownership and frees this RangeRequest.
-  auto rr = new RangeRequest();
-  rr->set_key(LastIDKey(chain_id));
-  rr->set_range_end("");
-  RequestOp get_last_id;
-  get_last_id.set_allocated_request_range(rr);
   // Transactions are used by etcd to guard operations based on a condition.
   // This says, "If I am holding the lock $lock_id, then find out the last ID that was assigned."
   std::vector<Compare> compare_lock_exists = {*etcd::txn::BuildKeyExistsComparison(lock_id)};
-  std::vector<RequestOp> success_ops = {get_last_id};
+  std::vector<RequestOp> success_ops = {*etcd::txn::BuildGetRequest(LastIDKey(chain_id_))};
   std::vector<RequestOp> failure_ops = {};
-  auto get_tx = etcd_client_->Transaction(
-      compare_lock_exists,
-      success_ops,
-      failure_ops
-  );
+  auto get_tx = etcd_client_->Transaction(compare_lock_exists, success_ops, failure_ops);
   CHECK(get_tx->succeeded()) << "Tried to get the last assigned ID without holding the join lock. Is etcd running?";
 
   RangeResponse response = get_tx->responses(0).response_range();
@@ -180,41 +190,189 @@ void MasterClient::AcquireID(std::string chain_id) {
     member_id_ = 0;
   }
 
-  // RequestOp takes ownership and frees this PutRequest.
-  auto pr = new PutRequest();
-  pr->set_key(LastIDKey(chain_id));
-  pr->set_value(std::to_string(member_id_));
-  RequestOp put_new_id;
-  put_new_id.set_allocated_request_put(pr);
-  success_ops = {put_new_id};
-  failure_ops = {};
   // This says, "If I am holding the lock, then write down in etcd that my ID was the last ID assigned."
-  auto put_tx = etcd_client_->Transaction(
-    compare_lock_exists,
-    success_ops,
-    failure_ops
-  );
+  success_ops = {*etcd::txn::BuildPutRequest(LastIDKey(chain_id_), std::to_string(member_id_))};
+  failure_ops = {};
+  auto put_tx = etcd_client_->Transaction(compare_lock_exists, success_ops, failure_ops);
   CHECK(put_tx->succeeded())
     << "Tried to claim a new ID (" << member_id_ << ") without holding the join lock. Is etcd running?";
 
   auto unlock_response = etcd_client_->Unlock(lock_id);
-  LOG(INFO) << "Acquired ID " << member_id_ << " in chain " << chain_id;
+  LOG(INFO) << "Acquired ID " << member_id_ << " in chain " << chain_id_;
 }
 
 /**
- * Register as a chain member by putting my unique member ID as a key under the chain's config.
- * For example, if I am joining chain "foobar", and this.member_id_ = 1, this puts the key "credis/foobar/1".
- * The key contents are currently unimportant, but could be used to store (for example) connection info.
- * The PRESENCE of the specific member key tells the master if this client is alive.
- * We include heartbeat_lease_id_ in the etcd call so it will disappear if we time out from etcd.
- * @param chain_id
+ * Register my member info by updating the value of my heartbeat key.
+ * The key disappears if the member times out from etcd.
  */
-void MasterClient::RegisterAsMember(std::string chain_id) {
+void MasterClient::RegisterMemberInfo() {
+  json my_conn_info = {
+      {"address", redis_addr_},
+      {"port", std::to_string(redis_port_)},
+      {"prev", prev_id_},
+      {"next", next_id_},
+      {"role", role_}
+  };
   auto put_key = etcd_client_->Put(
-      ConnInfoKey(chain_id),
-      "Yet for all that, flowers fall amid our regret and yearning, and hated weeds grow apace.",
-      // - Dogen, founder of the Soto Zen School, in the Genjokoan
+      HeartbeatKey(chain_id_, member_id_),
+      my_conn_info.dump(),
+      // include lease in the etcd call so that the key disappears if we time out from etcd.
       heartbeat_lease_id_
   );
+  LOG(INFO) << "Registered new connection info: " << my_conn_info.dump();
 }
 
+/**
+ * Start a thread to watch for changes to this member's configuration. If some are found, the thread updates the
+ * member's configuration by sending RESP commands on the loopback interface to the port given by OWN_PORT.
+ * @param chain_id
+ * @param own_port
+ */
+void MasterClient::StartWatchingConfig() {
+  // Starts a thread to watch for updated configuration and apply it to the member.
+  config_thread_ = std::unique_ptr<std::thread>(new std::thread(
+      // No need to join - this should only die if the process dies.
+      [this]() {
+          auto etcd = std::shared_ptr<EtcdClient>(new EtcdClient(channel_));
+          auto changes = etcd->WatchCreate(
+              ConfigKey(chain_id_, member_id_),
+              /*range_end=*/"",
+              /*start_revision=*/-1,
+              /*progress_notify=*/true,
+              /*filters=*/{},
+              /*prev_kv=*/false
+          );
+          LOG(INFO) << "Started watching configs at " << ConfigKey(chain_id_, member_id_);
+          WatchResponse wr;
+          changes->WaitForInitialMetadata();
+          changes->Read(&wr);
+          // The first response is to the CreateRequest.
+          int64_t last_rev_seen = wr.header().revision();
+          while (true) {
+            if (!changes->Read(&wr)) {
+              // changes = etcd->WatchCreate(ConfigKey(chain_id_, member_id_), "", -1, true, {}, false);
+              break;
+            }
+            // There would be 0 events if it was just a progress_notify
+            // (see https://coreos.com/etcd/docs/latest/learning/api.html#watch-streams)
+            if (wr.events_size() > 0) {
+              Event latest_event = wr.events().Get(wr.events_size() - 1);
+              last_rev_seen = latest_event.kv().mod_revision();
+              if (latest_event.type() == Event_EventType_PUT) {
+                HandleConfigPut(latest_event, etcd);
+              } else {
+                // TODO: leave the cluster if config file is deleted.
+              }
+            }
+          }
+      }
+    ));
+}
+
+void MasterClient::HandleConfigPut(Event put_event, std::shared_ptr<etcd::EtcdClient> etcd) {
+  CHECK(put_event.type() == Event_EventType_PUT) << "HandleConfigPut must be called on a PUT event";
+  auto my_config = json::parse(put_event.kv().value());
+  std::string my_role = my_config["role"];
+  std::string prev_id = my_config["prev"];
+  std::string next_id = my_config["next"];
+
+  if (my_role == "singleton") {
+    auto redisCtx = std::unique_ptr<redisContext>(redisConnect(redis_addr_.c_str(), redis_port_));
+    redisReply* reply = reinterpret_cast<redisReply*>(redisCommand(
+        redisCtx.get(),
+        "MEMBER.SET_ROLE %s %s %s %s %s %s %s",
+        my_role.c_str(), "nil", "nil", "nil", "nil", "-1", "0"
+    ));
+    freeReplyObject(reply);
+    return;
+  }
+
+  // Arguments to MEMBER.SET_ROLE.
+  std::string prev_address = "nil";
+  std::string next_address = "nil";
+  std::string prev_port = "nil";
+  std::string next_port = "nil";
+  std::string sn_string = "-1";
+  std::string drop_writes_string = "0";
+
+  // Build and run an etcd transaction to get reported config of the new head and new tail.
+  // If a heartbeat key we expected to be there doesn't exist, the transaction will not succeed.
+  std::vector<Compare> comparisons;
+  std::vector<RequestOp> success_ops;
+  std::vector<RequestOp> failure_ops = {};
+  bool has_next = (my_role != "tail" && my_role != "singleton");
+  bool has_prev = (my_role != "head" && my_role != "singleton");
+  std::string prev_hb_key, next_hb_key;
+  if (has_next) {
+    next_hb_key = HeartbeatKey(chain_id_, std::stol(next_id));
+    comparisons.push_back(*etcd::txn::BuildKeyExistsComparison(next_hb_key));
+    success_ops.push_back(*etcd::txn::BuildGetRequest(next_hb_key));
+  }
+  if (has_prev) {
+    prev_hb_key = HeartbeatKey(chain_id_, std::stol(prev_id));
+    comparisons.push_back(*etcd::txn::BuildKeyExistsComparison(prev_hb_key));
+    success_ops.push_back(*etcd::txn::BuildGetRequest(prev_hb_key));
+  }
+  auto tx = etcd->Transaction(comparisons, success_ops, failure_ops);
+  if (tx->succeeded()) {
+    // Parse config info out of the responses.
+    for (const auto &res : tx->responses()) {
+      auto res_kv = res.response_range().kvs().Get(0);
+      auto their_config = json::parse(res_kv.value());
+      if (res_kv.key() == prev_hb_key) {
+        prev_address = their_config["address"];
+        prev_port = their_config["port"];
+      } else if (res_kv.key() == next_hb_key) {
+        next_address = their_config["address"];
+        next_port = their_config["port"];
+      }
+    }
+  } else {
+    // TODO: retry with backoff.
+    LOG(INFO) << "Attempt to read new config failed. Is the new parent or new child down?";
+  }
+
+  // Block until the new successor updates its reported state.
+  if (has_next) {
+    int64_t backoff_sec = 1;
+    while (true) {
+      auto next_config_req = etcd->Range(next_hb_key, "", -1);
+      if (next_config_req->kvs_size() > 0) {
+        auto next_config = json::parse(next_config_req->kvs().Get(0).value());
+        if (next_config["prev"] == std::to_string(member_id_)) {
+          LOG(INFO) << "New child reports us as its parent - forwarding writes...";
+          break;
+        }
+      }
+      LOG(INFO) << "New child " << next_id << " has not reported us as its parent yet. "
+                << "Waiting " << backoff_sec << "sec";
+      std::this_thread::sleep_for(std::chrono::seconds(backoff_sec));
+      backoff_sec = backoff_sec * 2;
+    }
+  }
+
+  // Set my own role by sending myself a Redis command.
+  redisContext* redis_ctx = redisConnect(redis_addr_.c_str(), redis_port_);
+  auto reply = reinterpret_cast<redisReply*>(redisCommand(
+      redis_ctx,
+      "MEMBER.SET_ROLE %s %s %s %s %s %s %s",
+      my_role.c_str(),
+      prev_address.c_str(),
+      prev_port.c_str(),
+      next_address.c_str(),
+      next_port.c_str(),
+      sn_string.c_str(),
+      drop_writes_string.c_str()
+  ));
+
+  // If we were able to successfully set our own role, then update our config in etcd.
+  if (redis_ctx->err == 0) {
+    role_ = my_role;
+    prev_id_ = prev_id;
+    next_id_ = next_id;
+    RegisterMemberInfo();
+  } else {
+    LOG(INFO) << "MEMBER.SET_ROLE failed with error: " << redis_ctx->errstr;
+  }
+  freeReplyObject(reply);
+}
