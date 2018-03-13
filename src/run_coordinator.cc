a79b0315320c21702bfa4f3bc03a4cdbdca0bf40
Grayson Chao <grayson.chao@gmail.com>
2018-03-12 18:25:40 -0700

    Add flags to coordinator

--- src/run_coordinator.cc
+++ src/run_coordinator.cc
@@ -1,4 +1,5 @@
 #include <chrono>
+#include "gflags/gflags.h"
 #include "glog/logging.h"
 #include "grpcpp/grpcpp.h"
 #include "etcd/etcd.h"
@@ -7,11 +8,14 @@
 // Created by Grayson Chao on 3/2/18.
 //
 
+DEFINE_string(address, "127.0.0.1", "address of etcd");
+DEFINE_int32(port, 2379, "listen port of etcd");
+
 int main(int argc, char* argv[]) {
-  std::string address = "127.0.0.1";
-  int port = 2379;
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
   auto channel = grpc::CreateChannel(
-      address + ":" + std::to_string(port),
+      FLAGS_address + ":" + std::to_string(FLAGS_port),
       grpc::InsecureChannelCredentials()
   );
   auto etcd = std::unique_ptr<etcd::ClientInterface>(new etcd::Client(channel));
@@ -22,7 +26,8 @@ int main(int argc, char* argv[]) {
   }
   std::string chain_id(argv[1]);
   LOG(INFO)
-      << "Managing chain " << chain_id << " at " << address << ":" << port;
+      << "Managing chain " << chain_id
+      << " at " << FLAGS_address << ":" << FLAGS_port;
   grpc::Status status = c.ManageChain(chain_id);
   if (status.ok()) {
     LOG(FATAL) << status.error_message();
